081741801748951fb68f1faab34690f3c149a0d7
Trey Fordham <treyf711@gmail.com>
2018-01-11 05:25:38 -0500

    Working bind/unbind/start scripts

---
+++ qemuStart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+keyboard="04d9:a0cd"
+mouse="045e:0023"
+
+for i in {4..7}
+do
+    echo  performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+done
+
+taskset -c 4-7 qemu-system-x86_64 \
+    -display none \
+    -enable-kvm \
+    -M pc,accel=kvm \
+    -m 8192 \
+    -boot c \
+    -drive file=/mnt/storage/VM/win10.img,format=raw,if=virtio \
+    -drive file=/dev/sdb2,format=raw,if=virtio \
+    -cpu host,kvm=off \
+    -smp 4,sockets=1,cores=4,threads=1 \
+    -rtc base=localtime,driftfix=slew \
+    -device ioh3420,bus=pci.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root \
+    -device vfio-pci,host=01:00.0,bus=root,addr=00.0,romfile=/mnt/storage/VM/Ellesmere.rom,multifunction=on,x-vga=on -vga none \
+    -device vfio-pci,host=01:00.1,bus=root,addr=00.1 \
+    -device vfio-pci,host=00:14.2 \
+    -netdev bridge,id=bridger \
+    -device virtio-net-pci,netdev=bridger \
+    -usbdevice host:${keyboard} -usbdevice host:${mouse} 
